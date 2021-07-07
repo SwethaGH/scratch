ce2323daa22c865efa8198340ef612070a915cb6
SwethaGH <tangudubilliswethasri4302@gmail.com>
2021-07-07 06:14:10 +0530

    changes made

--- join.sql
+++ join.sql
@@ -2,16 +2,6 @@ SELECT * from customers inner join orders on customers.cust_id=orders.cust_id;
 select * from customers left join orders on customers.cust_id=orders.cust_id;
 select * from orders right outer join customers on customers.cust_id=orders.cust_id;
 
-
-
-
-
-
-
-
-
-
-
 select * from employee inner join department on employee.emp_id=department.emp_id;
 select employee.email,employee.gender,department.dept_loc from employee left join department on employee.emp_id=department.emp_id;
 select department.dept_id,department.dept_name from employee right join department on employee.emp_id=department.emp_id;
